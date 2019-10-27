c3f3b9334797637c8da65190e0a5f8bba94a0a79
fakeittymi <sserka@mail.ru>
2019-10-28 00:00:36 +0300

    изменены товары магазина(с книг на гитары)

---
+++ Swsu.Test/Swsu.Test.Asp/Controllers/CatalogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Swsu.Test.Asp.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index(int n)
+        {
+            ViewBag.Page = n;
+            return View();
+        }
+    }
+}
